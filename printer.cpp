7838053d66fd683f4ef805b284b07700447a65f1
Matthew Kelly <matt1.kelly@umontana.edu>
2020-05-24 03:33:06 -0600

    split cpp and hpp files

--- printer.cpp
+++ printer.cpp
@@ -1,28 +1,4 @@
-#include <iostream>
-#include <sstream>
-
-struct Printer
-{
-	std::string sep;
-	std::string end;
-	std::stringstream stream;
-
-	Printer(std::string sep = " ", std::string end = "\n") : sep(sep), end(end) {}
-
-	template <typename... arg_types>
-	void print(arg_types... args)
-	{
-		parse(args...);
-		std::cout << stream.str();
-	}
-	template <typename arg_t, typename... arg_types>
-	void parse(arg_t arg, arg_types... args)
-	{
-		stream << arg << sep;
-		parse(args...);
-	}
-	void parse() { stream << end; }
-};
+#include "printer.hpp"
 
 int main()
 {
