dfe2e540d4b826e7ee8793c5fdcb888a3f5f4a54
Matthew Kelly <matt1.kelly@umontana.edu>
2020-05-24 02:43:25 -0600

    working

---
+++ printer.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <sstream>
+
+struct Printer
+{
+    std::string sep;
+    std::string end;
+	std::stringstream stream;
+
+	Printer(std::string sep=" ", std::string end="\n"): sep(sep), end(end) {}
+
+    template<typename... arg_types>
+    void print(arg_types... args)
+    {
+		parse(args...);
+        std::cout << stream.str();
+    }
+
+	template <typename arg_t, typename... arg_types>
+	void parse(arg_t arg, arg_types... args)
+	{
+		stream << arg << sep;
+		parse(args...);
+	}
+
+	void parse() { stream << end; }
+};
+
+int main()
+{
+	Printer printer;
+    printer.print(3, 5);
+
+	return EXIT_SUCCESS;
+}
